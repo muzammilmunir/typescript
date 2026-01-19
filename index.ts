cfc056dd97608e7e698073fbcd29764d7370f6f1
muzammilmunir <muzammil_khania@hotmail.com>
2026-01-19 17:36:46 +0500

    type alias added

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
-// const birthdayGreeter = (name: string, age: number): string => {
-//   return `Happy birthday ${name}, you are now ${age} years old!`;
-// };
+type Operation = 'multiply' | 'add' | 'divide';
 
-// const birthdayHero = "Jane User";
-// const age = 22;
-
-// console.log(birthdayGreeter(birthdayHero, age));
-
-const multiplicator = (a: number, b: number, printText: string) => {
-  console.log(printText,  a * b);
+const calculator = (a: number, b: number, op: Operation) => {
+    if (op === 'multiply') {
+        return a * b;
+    } else if (op === 'add') {
+        return a + b;
+    } else if (op === 'divide') {
+        if (b === 0) return 'can\'t divide by 0!';
+        return a / b;
+    }
 }
 
-multiplicator('how about a string?', 4, 'Multiplied a string and 4, the result is:');
+calculator(1, 1, "yolo")
