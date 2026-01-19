0a10383e0c45f362d793d975b94fddce61338238
muzammilmunir <muzammil_khania@hotmail.com>
2026-01-19 16:33:58 +0500

    without type

--- index.ts
+++ index.ts
@@ -1,8 +1,14 @@
-const birthdayGreeter = (name: string, age: number): string => {
-  return `Happy birthday ${name}, you are now ${age} years old!`;
-};
+// const birthdayGreeter = (name: string, age: number): string => {
+//   return `Happy birthday ${name}, you are now ${age} years old!`;
+// };
 
-const birthdayHero = "Jane User";
-const age = 22;
+// const birthdayHero = "Jane User";
+// const age = 22;
 
-console.log(birthdayGreeter(birthdayHero, age));
+// console.log(birthdayGreeter(birthdayHero, age));
+
+const multiplicator = (a, b, printText) => {
+  console.log(printText,  a * b);
+}
+
+multiplicator(2, 4, 'Multiplied numbers 2 and 4, the result is:');
