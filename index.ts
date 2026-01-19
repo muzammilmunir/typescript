64830ac68cbf1f94d72104448911252f2e683663
muzammilmunir <muzammil_khania@hotmail.com>
2026-01-19 17:34:31 +0500

    with type fixes

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@
 
 // console.log(birthdayGreeter(birthdayHero, age));
 
-const multiplicator = (a, b, printText) => {
+const multiplicator = (a: number, b: number, printText: string) => {
   console.log(printText,  a * b);
 }
 
