567aebb33caaaee50789b50916806ea2f5aa456a
muzammilmunir <muzammil_khania@hotmail.com>
2026-01-19 20:49:35 +0500

    part b done

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { calculator } from './calculator';
+import { calculator, Operation } from './calculator';
 const app = express();
 
 app.get('/ping', (_req, res) => {
@@ -10,9 +10,12 @@ app.post('/calculate', (req, res) => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const { value1, value2, op } = req.body;
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-  const result = calculator(Number(value1), Number(value2), op);
-  res.send({ result });
+  if ( !value1 || isNaN(Number(value1)) ) {
+    return res.status(400).send({ error: '...'});
+  }
+
+  const result = calculator(Number(value1), Number(value2), op as Operation);
+  return res.send({ result });
 });
 
 const PORT = 3003;
